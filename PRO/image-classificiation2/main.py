c154b0e4108383d4a32343d4c37c02f30878347c
shibarin <shibarin317@icloud.com>
2023-03-30 23:53:41 +0900

    2023/03/30

--- PRO/image-classificiation2/main.py
+++ PRO/image-classificiation2/main.py
@@ -12,24 +12,29 @@ torch.manual_seed(seed)
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, 7, padding=3)
-        self.conv2 = nn.Conv2d(32, 64, 5, padding=2)
-        self.fc1 = nn.Linear(64*32*32, 128)
+        self.conv1 = nn.Conv2d(3, 64, 5, padding=2)
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.fc1 = nn.Linear(128*8*8, 128)
         self.fc2 = nn.Linear(128, 2)
-        # self.avg_pool = nn.AvgPool2d(2)
-        # self.max_pool = nn.MaxPool2d(2)
+        self.max_pool = nn.MaxPool2d(2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        x = self.max_pool(x)
         x = F.relu(self.conv2(x))
-        x = x.reshape([64*32*32])
+        x = self.max_pool(x)
+        # print(x.shape)
+        x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+        x = F.log_softmax(x, dim=1)
         return x
 
 model = CNN()
 
-#誤差関数 : NLLLossを使用
+device = torch.device("cuda")
+
+#誤差関数 : nll_lossを使用
 criterion = F.nll_loss
 
 # optimizer : SGDを使用
@@ -52,24 +57,29 @@ train_data = torch.from_numpy(train_data).float()
 train_label = torch.from_numpy(train_label).long()
 validation_data = torch.from_numpy(validation_data).float()
 
-for epoch in range(5):
+for epoch in range(300):
+    #データ全ての合計のロス
+    running_loss = 0.0
     # optimizerの初期化
     optimizer.zero_grad()
     outputs = model(train_data)
     loss = criterion(outputs, train_label)
     loss.backward()
     optimizer.step()
+    running_loss += loss.item()
+    if epoch % 50 == 0:
+        print(epoch, running_loss)
 
 # 検証データの推論
 with torch.no_grad():
     outputs = model(validation_data)
     _, predicted = torch.max(outputs.data, 1)
 
-# predicted = predicted.tolist()
+predicted = predicted.tolist()
 
 print(predicted)
 
-# f = open("submission.csv", 'w')
-# f.write('id, label\n')
-# for i in range(1, 136):
-#     f.write(str(i) + ', ' + str(predicted[i-1]) + '\n')
+f = open("submission.csv", 'w')
+f.write('id, label\n')
+for i in range(1, 112):
+    f.write(str(i) + ', ' + str(predicted[i-1]) + '\n')
